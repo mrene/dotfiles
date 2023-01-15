74fbbfb5123555d43f07ed436bb69cfbcc4dcf59
Mathieu Rene <mathieu.rene@gmail.com>
2023-01-15 14:10:51 +0000

    wip nixos config

--- flake.nix
+++ flake.nix
@@ -35,8 +35,8 @@
     flake-utils.lib.eachDefaultSystem
       (system:
         let
-          pkgs = import nixpkgs { 
-            inherit system; 
+          pkgs = import nixpkgs {
+            inherit system;
             config = {
               permittedInsecurePackages = [
                 "python3.10-poetry-1.2.2" # CVE-2022-42966 - Regex DoS
@@ -55,85 +55,124 @@
 
         })
     // # <- concatenates Nix attribute sets
-    (let pkgsConfig = {
+    (
+      let
+        pkgsConfig = {
           permittedInsecurePackages = [
             "python3.10-poetry-1.2.2" # CVE-2022-42966 - Regex DoS
           ];
         };
-        in
-    {
-      homeConfigurations = {
-        "mrene@beast" = inputs.home-manager.lib.homeManagerConfiguration {
-          # pkgs = inputs.nixpkgs.legacyPackages.x86_64-linux;
-          pkgs = import nixpkgs { 
-            system = "x86_64-linux";
-            config = pkgsConfig;
+      in
+      {
+        homeConfigurations = {
+          "mrene@beast" = inputs.home-manager.lib.homeManagerConfiguration {
+            # pkgs = inputs.nixpkgs.legacyPackages.x86_64-linux;
+            pkgs = import nixpkgs {
+              system = "x86_64-linux";
+              config = pkgsConfig;
+            };
+            modules = [
+              ./nixpkgs/home-manager/beast.nix
+              ({ pkgs, ... }: {
+                home.packages = [
+                  inputs.minidsp.packages.${pkgs.system}.default
+                  inputs.devenv.packages.${pkgs.system}.devenv
+                ];
+              })
+            ];
+            # extraSpecialArgs = { 
+            #   pkgsUnstable = inputs.nixpkgs.legacyPackages.aarch64-darwin;
+            # };
           };
-          modules = [
-            ./nixpkgs/home-manager/beast.nix
-            ({pkgs, ...}: {
-              home.packages = [
-                inputs.minidsp.packages.${pkgs.system}.default
-                inputs.devenv.packages.${pkgs.system}.devenv
-              ];
-            })
-          ];
-          # extraSpecialArgs = { 
-          #   pkgsUnstable = inputs.nixpkgs.legacyPackages.aarch64-darwin;
-          # };
+
+          "mrene@utm" = inputs.home-manager.lib.homeManagerConfiguration {
+            pkgs = import nixpkgs {
+              system = "aarch64-linux";
+              config = pkgsConfig;
+            };
+            modules = [
+              ./nixpkgs/home-manager/utm.nix
+              ({ pkgs, ... }: {
+                home.packages = [
+                  inputs.minidsp.packages.${pkgs.system}.default
+                  inputs.devenv.packages.${pkgs.system}.devenv
+                ];
+              })
+            ];
+            # extraSpecialArgs = { 
+            #   pkgsUnstable = inputs.nixpkgs.legacyPackages.aarch64-darwin;
+            # };
+          };
+
+          "mrene@Mathieus-MBP" = inputs.home-manager.lib.homeManagerConfiguration {
+            pkgs = import nixpkgs {
+              system = "aarch64-darwin";
+              config = pkgsConfig;
+            };
+            modules = [
+              ./nixpkgs/home-manager/mac.nix
+            ];
+            # extraSpecialArgs = { pkgsUnstable = inputs.nixpkgs.legacyPackages.aarch64-darwin; };
+          };
         };
 
-        "mrene@Mathieus-MBP" = inputs.home-manager.lib.homeManagerConfiguration {
-          # pkgs = inputs.nixpkgs.legacyPackages.aarch64-darwin;
-          pkgs = import nixpkgs { 
+        darwinConfigurations = {
+          # nix build .#darwinConfigurations.mbp2021.system
+          # ./result/sw/bin/darwin-rebuild switch --flake .
+          Mathieus-MBP = darwin.lib.darwinSystem {
             system = "aarch64-darwin";
-            config = pkgsConfig;
+            modules = [
+              ./nixpkgs/darwin/mbp2021/configuration.nix
+              home-manager.darwinModules.home-manager
+              {
+                # `home-manager` config
+                home-manager.useGlobalPkgs = true;
+                home-manager.useUserPackages = true;
+                home-manager.verbose = true;
+                home-manager.users.mrene = import ./nixpkgs/home-manager/mac.nix;
+              }
+            ];
+            inputs = { inherit darwin nixpkgs; };
           };
-          modules = [
-            ./nixpkgs/home-manager/mac.nix
+        };
+
+        nixosConfigurations = {
+          # sudo nixos-rebuild switch --flake .#homepi
+          homepi = inputs.nixpkgs.lib.nixosSystem {
+            system = "aarch64-linux";
+            specialArgs = { common = self.common; inherit inputs; };
+            modules = [ ];
+          };
+
+          # sudo nixos-rebuild switch --flake .#utm
+          utm = inputs.nixpkgs.lib.nixosSystem {
+            system = "aarch64-linux";
+            specialArgs = { common = self.common; inherit inputs; };
+            modules = [
+              ./nixpkgs/nixos/utm/configuration.nix
+              home-manager.nixosModules.home-manager
+              {
+                # `home-manager` config
+                home-manager.useGlobalPkgs = true;
+                home-manager.useUserPackages = true;
+                home-manager.verbose = true;
+                home-manager.users.mrene = import ./nixpkgs/home-manager/utm.nix;
+              }
+            ];
+
+          };
+        };
+
+        images = {
+          # nix build .#images.homepi
+          homepi = self.nixosConfigurations.homepi.config.system.build.sdImage;
+        };
+
+        common = {
+          sshKeys = [
+            "ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBMDK9LCwU62BIcyn73TcaQlMqr12GgYnHYcw5dbDDNmYnpp2n/jfDQ5hEkXd945dcngW6yb7cmgsa8Sx9T1Uuo4= secretive@mbp2021"
           ];
-          # extraSpecialArgs = { pkgsUnstable = inputs.nixpkgs.legacyPackages.aarch64-darwin; };
         };
-      };
-
-      darwinConfigurations = {
-        # nix build .#darwinConfigurations.mbp2021.system
-        # ./result/sw/bin/darwin-rebuild switch --flake .
-        Mathieus-MBP = darwin.lib.darwinSystem {
-          system = "aarch64-darwin";
-          modules = [
-            ./nixpkgs/darwin/mbp2021/configuration.nix
-            home-manager.darwinModules.home-manager
-            {
-              # `home-manager` config
-              home-manager.useGlobalPkgs = true;
-              home-manager.useUserPackages = true;
-              home-manager.verbose = true;
-              home-manager.users.mrene = import ./nixpkgs/home-manager/mac.nix;
-            }
-          ];
-          inputs = { inherit darwin nixpkgs; };
-        };
-      };
-
-      nixosConfigurations = {
-        # sudo nixos-rebuild switch --flake .#homepi
-        homepi = inputs.nixpkgs.lib.nixosSystem {
-          system = "aarch64-linux";
-          specialArgs = { common = self.common; inherit inputs; };
-          modules = [ ];
-        };
-      };
-
-      images = {
-        # nix build .#images.homepi
-        homepi = self.nixosConfigurations.homepi.config.system.build.sdImage;
-      };
-      
-      common = {
-        sshKeys = [
-          "ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBMDK9LCwU62BIcyn73TcaQlMqr12GgYnHYcw5dbDDNmYnpp2n/jfDQ5hEkXd945dcngW6yb7cmgsa8Sx9T1Uuo4= secretive@mbp2021"
-        ];
-      };
-    });
+      }
+    );
 }
