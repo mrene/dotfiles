1412e6e9286510bd9fc62bac7bb48933eac247fa
Mathieu Rene <mathieu.rene@gmail.com>
2023-02-05 13:23:51 -0500

    cleanup

--- nixpkgs/darwin/mbp2021/configuration.nix
+++ nixpkgs/darwin/mbp2021/configuration.nix
@@ -24,26 +24,7 @@
     experimental-features = nix-command flakes
   '';
 
-  # nix = {
-  #   buildMachines = lib.filter (x: x.hostName != config.networking.hostName) [
-  #     {
-  #       systems = [ "aarch64-linux" "x86_64-linux" ];
-  #       sshUser = "root";
-  #       maxJobs = 4;
-  #       # relies on `/var/root/.ssh/nix-builder` key to be there
-  #       # TODO set this up via nix
-  #       hostName = "oracle-nix-builder";
-  #       supportedFeatures = [ "nixos-test" "benchmark" "kvm" "big-parallel" ];
-  #     }
-  #   ];
-  #   distributedBuilds = config.nix.buildMachines != [ ];
-  # };
-
-  # Setup 1Password CLI `op`
-  nixpkgs.config.allowUnfreePredicate = pkg: builtins.elem (lib.getName pkg) [
-    "1password-cli"
-  ];
-
+  nixpkgs.config.allowUnfree = true;
   system.activationScripts.extraActivation.text = ''
     # For TouchID to work in `op` 1Password CLI, it needs to be at `/usr/local/bin`
     # (Hopefully this requirement will be lifted by 1Password at some point)
