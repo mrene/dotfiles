a945e4125ed6bb9028dd199531f5307a75d166f7
Mathieu Rene <mathieu.rene@gmail.com>
2023-02-05 09:15:51 -0500

    up

--- nixpkgs/nixos/utm/configuration.nix
+++ nixpkgs/nixos/utm/configuration.nix
@@ -29,7 +29,7 @@
   networking.hostName = "utm-nixos";
   networking.networkmanager.enable = true; # Easiest to use and most distros use this by default.
 
-  #services.xserver.enable = true;
+  services.xserver.enable = true;
   #services.xserver.desktopManager.gnome.enable = true;
   #services.xserver.displayManager.gdm.enable = true;
 
