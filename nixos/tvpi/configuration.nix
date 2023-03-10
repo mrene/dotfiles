654796a60c243cabd6b5cbfd2104fb966a526980
Mathieu Rene <mathieu.rene@gmail.com>
2023-03-10 12:26:01 -0500

    tvpi working

--- nixos/tvpi/configuration.nix
+++ nixos/tvpi/configuration.nix
@@ -35,7 +35,7 @@
   hardware.enableRedistributableFirmware = true;
 
   # Enables the generation of /boot/extlinux/extlinux.conf
-  #boot.loader.generic-extlinux-compatible.enable = true;
+  boot.loader.generic-extlinux-compatible.enable = true;
 
   # !!! If your board is a Raspberry Pi 1, select this:
   #boot.kernelPackages = pkgs.linuxPackages_rpi;
@@ -43,7 +43,20 @@
   # Use the systemd-boot EFI boot loader.
   boot.loader.timeout = 5;
 
-  networking.hostName = "tvpi";
+  services.openssh.enable = true;
+
+  networking = {
+    hostName = "tvpi";
+    usePredictableInterfaceNames = false;
+
+    interfaces.eth0.ipv4.addresses = [{
+      address = "192.168.1.245";
+      prefixLength = 24;
+    }];
+
+    defaultGateway = "192.168.1.1";
+    nameservers = [ "1.1.1.1" "8.8.8.8" ];
+  };
 
   users.users.root.openssh.authorizedKeys.keys = common.sshKeys;
   users.defaultUserShell = pkgs.fish;
@@ -57,12 +70,16 @@
     openssh.authorizedKeys.keys = common.sshKeys;
   };
 
+  sdImage = {
+    firmwareSize = 100;
+    compressImage = false;
+  };
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
   # this value at the release version of the first install of this system.
   # Before changing this value read the documentation for this option
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "22.11"; # Did you read the comment?
+  system.stateVersion = "23.05"; # Did you read the comment?
 }
 
