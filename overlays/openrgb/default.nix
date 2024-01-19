d72da18b74def76907a608f72e4cebe894930d3c
Mathieu Rene <mathieu.rene@gmail.com>
2024-01-19 17:11:48 -0500

    chore(deps): bump openrgb

--- overlays/openrgb/default.nix
+++ overlays/openrgb/default.nix
@@ -1,10 +1,10 @@
 (_final: prev: {
   openrgb = prev.openrgb.overrideAttrs (old: {
-    src = prev.fetchFromGitLab {
+    src = prev.fetchFromGitHub {
       owner = "CalcProgrammer1";
       repo = "OpenRGB";
-      rev = "baf5d30b27fe5125d17a0df57838eac4234ff01b"; #pin
-      sha256 = "0nw1adnyha4rdd37pgl9izhyg8pcmmd4h00cgv3pmmhc3kg5ryha";
+      rev = "6c279ea830e5442fc409a958b979fb8949073b2e";
+      hash = "sha256-Il3EpVsM5FjzpH5ayplz1ezCeV+cY0LPF/66+US8RGY=";
     };
     patches = [ ./0001-sd_notify.patch ];
     buildInputs = old.buildInputs ++ [ prev.systemd ];
