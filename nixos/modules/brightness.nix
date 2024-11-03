8dee7927a8298e04c4d953f03f2f709589d38ded
Mathieu Rene <mathieu.rene@gmail.com>
2024-11-03 10:47:06 -0500

    up

--- nixos/modules/brightness.nix
+++ nixos/modules/brightness.nix
@@ -46,16 +46,22 @@ in
     environment.systemPackages = [ pkgs.ddcutil ];
     systemd.services.ddcci-setup = {
       description = "DDCCI Setup";
-      before = [ "display-manager.service" ];
+      wantedBy = [ "display-manager.service" ];
       serviceConfig = {
-      Type = "oneshot";
-      ExecStart = ''
-        ${lib.getExe pkgs.ddcutil} detect | ${lib.getExe pkgs.ripgrep} -o 'i2c-\\d+' | while read dev; do 
-          echo ddcci 0x37 > /sys/bus/i2c/devices/''${dev}/new_device; 
-        done
-      '';
+        Type = "oneshot";
+        ExecStart = let
+          ddcciSetupScript = pkgs.writeShellScript "ddcci-setup" ''
+            # ${lib.getExe pkgs.ddcutil} --disable-dynamic-sleep detect | ${lib.getExe pkgs.ripgrep} -o 'i2c-\d+' | while read dev; do 
+            #   echo "Enabling DDCCI on ''${dev}"
+            #   echo ddcci 0x37 > /sys/bus/i2c/devices/''${dev}/new_device; 
+            # done
+            echo ddcci 0x37 > /sys/bus/i2c/devices/i2c-5/new_device;
+            sleep 2
+            echo ddcci 0x37 > /sys/bus/i2c/devices/i2c-6/new_device;
+            echo "Done"
+          '';
+        in "${ddcciSetupScript}";
       };
-      install.wantedBy = [ "multi-user.target" ];
     };
 
     boot.extraModprobeConfig = ''
