91a9e88353016d316dc39134d98d6ef933406587
Mathieu Rene <mathieu.rene@gmail.com>
2025-05-25 23:48:11 -0400

    restore default.nix

---
+++ _sources/default.nix
@@ -0,0 +1,7 @@
+{callPackage}: let
+  sources = callPackage ./generated.nix {};
+in {
+  inherit sources;
+  dockerImageUrl = name: sources.${name}.src.sourceURL;
+  fetch = name: sources.${name}.src;
+}
