8e185a5895bd10f1b555bd782f515c35a5da1e3f
Mathieu Rene <mathieu.rene@gmail.com>
2026-05-17 21:40:10 -0400

    migrate git/jj configs from shared-dotfiles into aspects

--- aspects/dev/git.nix
+++ aspects/dev/git.nix
@@ -45,5 +45,40 @@ _: {
           features = "chameleon-mod";
         };
       };
+
+      # fzf-driven git helpers. See https://github.com/junegunn/fzf-git.sh/blob/main/fzf-git.sh
+      programs.fish.functions = {
+        gb = ''
+          set COMMAND 'git for-each-ref --sort=-committerdate refs/heads/ --format="%(color: red)%(committerdate:short)%(color: 244)|%(color: cyan)%(refname:short)%(color: 244)|%(color: green)%(subject)" --color=always | column -ts"|"'
+          FZF_DEFAULT_COMMAND=$COMMAND fzf \
+            --ansi \
+            --header "enter to checkout, ctrl-d to delete" \
+            --bind "ctrl-d:execute(echo {+} | awk '{print \$2}' | xargs git branch -D)+reload:$COMMAND" \
+            | awk '{print $2}' | xargs git checkout
+        '';
+
+        gcpm = ''
+          MSG=(git log --first-parent --pretty=format:%s | head -n 100 | uniq | head -n 10 | fzf) git commit -m "$MSG"
+        '';
+
+        gh-pr-select = ''
+          set COMMAND 'gh pr list --json number,title,author,headRefName,updatedAt \
+          --template "{{tablerow \"Ref\" \"PR\" \"Title\" \"Author\" \"Date\"}}{{range .}}{{tablerow (.headRefName | color \"blue\") (printf \"#%v\" .number | color \"yellow\") (.title | color \"green\") (.author.name | color \"cyan\") (timeago .updatedAt)}}{{end}}"'
+          GH_FORCE_TTY=100% FZF_DEFAULT_COMMAND=$COMMAND fzf \
+                    --ansi \
+                    --header-lines=1 \
+                    --no-multi \
+                    --prompt 'Search Open PRs > ' \
+                    | awk '{print $1}'
+        '';
+
+        git-tag-select = ''
+          set COMMAND "git tag -l --sort=-version:refname --color=always --format='%(color:red)%(refname:short) %(color: cyan) - %(color: green)%(creatordate:short)'"
+          FZF_DEFAULT_COMMAND=$COMMAND fzf \
+                  --ansi \
+                  --prompt 'Search tags > ' \
+                  | awk '{print $1}'
+        '';
+      };
     };
 }
--- aspects/dev/jujutsu.nix
+++ aspects/dev/jujutsu.nix
@@ -1,7 +1,89 @@
 _: {
   flake.aspects.dev.homeManager =
-    { ... }:
+    { pkgs, ... }:
     {
+      # Exposed as shell scripts (not fish functions) so they're callable from
+      # non-fish contexts like scripts and Claude.
+      home.packages = with pkgs; [
+        (writeShellScriptBin "jj-main-branch" ''
+          jj log --no-graph -r 'trunk()' -T 'coalesce(local_bookmarks)'
+        '')
+        (writeShellScriptBin "jj-current-branch" ''
+          jj --ignore-working-copy log --no-graph -r "closest_bookmark(@)" -T 'local_bookmarks.map(|b| b.name()).join(",")'
+        '')
+        (writeShellScriptBin "jj-prev-branch" ''
+          jj-stacked-branches | head -n 2 | tail -n 1
+        '')
+        (writeShellScriptBin "jj-diff-working" ''
+          jj diff -r "$(jj-current-branch)..@" "$@"
+        '')
+        (writeShellScriptBin "jj-diff-branch" ''
+          jj diff -r "$(jj-prev-branch)..@" "$@"
+        '')
+        (writeShellScriptBin "jj-stacked-branches" ''
+          jj log --no-graph -r '(trunk()..@ | trunk()) & bookmarks()' -T 'coalesce(local_bookmarks) ++ "\n"' | sed 's/ *\*$//'
+        '')
+        (writeShellScriptBin "jj-stacked-stats" ''
+          if [ -n "$1" ]; then
+              from="$1"
+          else
+              from="trunk()"
+          fi
+          trunk=$(jj-main-branch)
+          echo "Changes since $from:"
+          jj log --reversed -r "$from..@" --no-graph -T 'change_id ++ "\n"' | while read -r change; do
+              # Exclude trunk
+              if [ "$change" = "$trunk" ]; then
+                  continue
+              fi
+
+              jj log -r "$change"
+              jj diff --stat -r "$change"
+              echo -e "\n"
+          done
+        '')
+      ];
+
+      programs.fish = {
+        functions = {
+          jj-select = ''
+            jj log --no-graph -T 'change_id.shortest() ++ "\t" ++ author.timestamp().ago() ++ " " ++ description.first_line() ++ " "  ++ bookmarks.join("  ") ++ "\n"' --color always | fzf --ansi --height 40% --layout reverse --border --preview 'jj diff --stat -r {1}' | cut -f1
+          '';
+
+          jj-b-select = ''
+            jj log --no-graph -r 'bookmarks()' -T 'coalesce(local_bookmarks) ++ "\n"' --color always | sed 's/ *\\*$//' | fzf --ansi | cut -f1
+          '';
+        };
+
+        shellAbbrs = {
+          jjpr = {
+            expansion = "gh pr create --head (jj-current-branch) --draft --body \"\" --title \"%\"";
+            setCursor = true;
+          };
+          jjspr = {
+            expansion = "gh pr create --base (jj-prev-branch) --head (jj-current-branch) --draft --body \"\" --title \"%\"";
+            setCursor = true;
+          };
+          jjrt = "jj rebase-trunk";
+          jjsi = "jj squash -t (jj-select) -i";
+          jjsw = "jj squash-working";
+          jjci = "jj commit -i";
+          jjcm = {
+            expansion = "jj commit -m \"%\"";
+            setCursor = true;
+          };
+          jjcmi = {
+            expansion = "jj commit -i -m \"%\"";
+            setCursor = true;
+          };
+        };
+
+        interactiveShellInit = ''
+          # Bind Ctrl+Alt+J to select a jj revision
+          bind -M insert ctrl-alt-j 'commandline -i (jj-select)'
+        '';
+      };
+
       programs.jujutsu = {
         enable = true;
 
@@ -17,6 +99,50 @@ _: {
             default-command = [
               "log"
               "--reversed"
+            ];
+          };
+
+          git = {
+            private-commits = "private()"; # prevent pushing private commits
+          };
+
+          revset-aliases = {
+            "closest_bookmark(to)" = "heads(::to & bookmarks())";
+            "recent()" = "committer_date(after:\"1 months ago\")";
+            "private()" = "description(glob:'private:*')";
+            "claude()" = "description(glob:'private: claude:*')";
+          };
+
+          aliases = {
+            # Move the most advanced bookmark between trunk and @- forward to the latest
+            # non-private change. Uses `trunk()..` (not `::`) to exclude merge side-branches.
+            # --from: find the head bookmark in trunk..@-, unioning trunk itself since `..` excludes it.
+            # --to: head of trunk..@- after removing private changes and their descendants.
+            "tug" = [
+              "bookmark"
+              "move"
+              "--from"
+              "heads(((trunk()..@-) | trunk()) & bookmarks())"
+              "--to"
+              "heads((trunk()..@-) ~ ((private() & (trunk()..@-))::))"
+            ];
+
+            # Rebase current branch onto trunk with support for multi-parents
+            "rebase-trunk" = [
+              "rebase"
+              "-s"
+              "roots(trunk()..@)" # root of any branches that leads us to trunk allowing support for multi-parents
+              "-d"
+              "trunk()" # rebase on trunk
+            ];
+
+            # Squash consecutive working changes (claude's, empty, undescribed) into @
+            "squash-working" = [
+              "squash"
+              "--from"
+              "(trunk()..@) & latest((trunk()..@) & ~(empty() | claude() | description(exact:'')))..(@- & (empty() | claude() | description(exact:'')))"
+              "--to"
+              "@"
             ];
           };
         };
